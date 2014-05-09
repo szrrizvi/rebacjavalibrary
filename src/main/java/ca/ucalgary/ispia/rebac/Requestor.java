8cf5904e9e00f6495a328604e9f59194504f7260
szrrizvi <szrrizvi@3c31df0f-8472-4393-ac21-792890e311ea>
2014-05-09 21:12:57 +0000

    Requestor is now a subinterface of Variable. Added Owner variant, also subinterface of Variable. -Zain

--- src/main/java/ca/ucalgary/ispia/rebac/Requestor.java
+++ src/main/java/ca/ucalgary/ispia/rebac/Requestor.java
@@ -1,13 +1,17 @@
 /**
  * Copyright (c) 2013 ReBAC Java Library
+
  * 
+
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
  * persons to whom the Software is furnished to do so, subject to the following conditions:
+
  * 
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
  * Software.
+
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -17,15 +21,27 @@
 
 package ca.ucalgary.ispia.rebac;
 
+
+
 /**
+
  * @author Syed Zain Rizvi
+
  */
+
+
 
 /**	
+
  *  Requestor variant for the interface {@link Policy}. Asserts that the requestor is the
+
  *  owner itself.<p>
+
  *  The purpose of this interface is to act as a marker
+
  */
-public interface Requestor extends Policy {
+
+public interface Requestor extends Variable {
 
 }
+
